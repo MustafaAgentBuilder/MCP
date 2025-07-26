9ab9c5e4eba9e4f0ec9b9fb1b807601cc6cba0e4
Mustafaadeel1 <mustafaadeel989@gmail.com>
2025-07-26 15:00:04 +0500

    " Who to make Tools in MCp Server "

---
+++ MCP basic/Mcp_Server/client.py
@@ -0,0 +1,49 @@
+import requests
+
+# url = "http://127.0.0.1:8000/mcp/"    # trailing slash is important
+
+# headers = {
+#     "Accept": "application/json,text/event-stream"
+# }
+
+# # In thos Body we say show all tools in MCP\server 
+
+# body = {
+#     "jsonrpc": "2.0",
+#     "method": "tools/list",          # matches @mcp.tool() name
+#     "id": 1,
+#     "params": {}
+# }
+
+# response = requests.post(url, headers=headers, json=body)
+
+# # for line in response.iter_lines():
+# #     if line:
+# #         print(line)
+
+# print(response.text)
+
+
+
+# # Here we see who can we call The Tool in Our and MCP server
+url = "http://127.0.0.1:8000/mcp/"  # ✅ this works now!
+
+headers = {
+    "Accept": "application/json,text/event-stream"
+}
+
+body = {
+  "id": 9,
+  "jsonrpc": "2.0",
+  "method": "tools/call",
+  "params": {
+    "session_id": "user_123",
+    "name": "read_doc",
+    "argument": {
+      "doc_id": "plan.md"
+    }
+  }
+}
+
+response = requests.post(url, headers=headers, json=body)
+print(response.text)
